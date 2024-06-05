309c62a64b4b30c205f9fe05d0edca8e5f107c9f
Shinjan sarkar <126502527+shinjansarkar@users.noreply.github.com>
2024-06-06 03:05:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function generatePassword() {
+    const mode = document.getElementById('mode').value;
+    let charset;
+    const length = 8;
+
+    if (mode === 'easy') {
+        charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    } else if (mode === 'medium') {
+        charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    } else if (mode === 'hard') {
+        charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    document.getElementById('password').value = password;
+}
