7ce1af3ce69b981deb0f44593206e63c87e47402
Shinjan sarkar <126502527+shinjansarkar@users.noreply.github.com>
2024-06-06 07:02:41 +0000

    bug fix

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function generatePassword() {
 
     document.getElementById('password').value = password;
 }
+
+function copyPassword() {
+    const passwordField = document.getElementById('password');
+    passwordField.select();
+    passwordField.setSelectionRange(0, 99999); // For mobile devices
+
+    navigator.clipboard.writeText(passwordField.value)
+        .then(() => {
+            alert('Password copied to clipboard');
+        })
+        .catch(err => {
+            alert('Failed to copy password: ' + err);
+        });
+}
